47a67359f02b233ed2616cc4069cf3257f12644a
lyjdwh <1412511544@qq.com>
2020-04-17 22:29:04 +0800

    patch lukesmith-externalpipe

--- st.h
+++ st.h
@@ -110,6 +110,9 @@ void *xmalloc(size_t);
 void *xrealloc(void *, size_t);
 char *xstrdup(char *);
 
+// from @LukeSmithxyz
+void externalpipe(const Arg *);
+
 /* config.h globals */
 extern char *utmp;
 extern char *stty_args;
