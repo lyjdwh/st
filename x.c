2340d6a239c0e7b25ac17e3dfd621507eabebe93
lyjdwh <1412511544@qq.com>
2021-01-26 22:49:21 +0800

    add clipboard patch

--- x.c
+++ x.c
@@ -680,6 +680,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
@@ -698,7 +700,10 @@ brelease(XEvent *e)
 
 	if (mouseaction(e, 1))
 		return;
-	if (e->xbutton.button == Button1)
+
+ 	if (e->xbutton.button == Button3)
+        clippaste(NULL);
+	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
 
